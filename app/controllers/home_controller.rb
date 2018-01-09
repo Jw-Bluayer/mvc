e4c0a7aa31a89b211bb4e0cc1a12f8269625e3c2
Jw-Bluayer <wjddn0728@naver.com>
2018-01-09 11:39:00 +0900

    MVC random game done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-  	@msg = "Hack your life!"
+
+  end
+
+  def result
+  	results = ['바보', '멍청이', '착함', '순수함', '나쁨', '귀여움']
+  	@result = results.sample(3)
+  	@spoon = (1..5).to_a.sample(3)
+  	@name = params[:username]
   end
 end
